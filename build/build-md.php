9028efabcaa2f2091fad9c1199234aa1e25028a9
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-06-06 10:36:44 +0200

    Update build-md.php

--- build/build-md.php
+++ build/build-md.php
@@ -16,8 +16,8 @@ $ideal_issuer_service = new IDealIssuerService();
 
 $ideal_issuers = $ideal_issuer_service->get_issuers();
 
-echo '| Code | Name | Mollie `32` `×` `24` SVG |', PHP_EOL;
-echo '| ---- | ---- | ------------------------ |', PHP_EOL;
+echo '| Code | Name | Mollie `32` `×` `24` SVG | iDEAL Hub `40` `×` `40` SVG |', PHP_EOL;
+echo '| ---- | ---- | ------------------------ |---------------------------- |', PHP_EOL;
 
 foreach ( $ideal_issuers as $ideal_issuer ) {
 	echo '| `', $ideal_issuer->code, '` | ', $ideal_issuer->name, ' | ';
@@ -30,10 +30,21 @@ foreach ( $ideal_issuers as $ideal_issuer ) {
 		$url = 'https://cdn.jsdelivr.net/npm/@wp-pay/logos@2.1.0' . $rel_path;
 
 		echo '![', $ideal_issuer->name, '](', $url, ')';
-		echo ' |';
 	}
 
-	echo PHP_EOL;
+	echo ' | ';
+
+	if ( \array_key_exists( 'ideal-hub-40x40.svg', $ideal_issuer->images ) ) {
+		$path = $ideal_issuer->images['ideal-hub-40x40.svg'];
+
+		$rel_path = \substr( $path, \strlen( $wp_pay_logos_path ) );
+
+		$url = 'https://cdn.jsdelivr.net/npm/@wp-pay/logos@2.1.0' . $rel_path;
+
+		echo '![', $ideal_issuer->name, '](', $url, ')';
+	}
+
+	echo ' |', PHP_EOL;
 }
 
 echo PHP_EOL;
